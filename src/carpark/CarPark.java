6da66581aaf586556d113e92152f366a4d7e2e05
MW5 <m.wojcik1988@gmail.com>
2016-12-10 09:37:03 +0100

    Switching between views works

--- src/carpark/CarPark.java
+++ src/carpark/CarPark.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package carpark;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -17,20 +9,10 @@ import javafx.stage.Stage;
  * @author MW5
  */
 public class CarPark extends Application {
-    
     @Override
     public void start(Stage stage) throws Exception {
-        StateManager r = new StateManager();
-        
-
-        //creates a group of elements
-        Group root = new Group();
-
-        
-        Scene scene = new Scene(r.prepareView(root));
-        stage.setTitle("Program do zarządzania przestrzenią parkingową");
-        stage.setScene(scene);
-        stage.show();
+        ViewController vC = new ViewController();
+        vC.init(stage);
     }
 
     public static void main(String[] args) {
